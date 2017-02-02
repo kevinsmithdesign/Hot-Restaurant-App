8fc29891f6d3c0b506ea2e3482ef8de222a446d7
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2017-02-02 16:38:28 -0600

    added routes and data. Also added html to secondary pages.

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080
 
-app.get('/', function(req,res){
-	res.send('Hello World');
-});
 
 //parse application/x-www-form-urlcoded
 app.use(bodyParser.urlencoded({ extended: false}))
@@ -14,13 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false}))
 //parse application/json
 app.use(bodyParser.json())
 
-app.use(function(req,res){
-	res.setHeader('Content-Type', 'text/plain');
-	res.write('you posted:\n');
-	res.end(JSON.stringify(req.body,null,2));
-});
 
-//app.listen(3000);
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+
 
 app.listen(PORT, function(){
 	console.log('App listening on PORT: ' + PORT);
